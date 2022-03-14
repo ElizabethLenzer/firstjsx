ea0d2caddfb6133952f1920beb3852dc37d0fa51
John Doe <elizabeth.lenzer1998@gmail.com>
2022-03-13 17:30:16 -0700

    ok

--- src/App.js
+++ src/App.js
@@ -21,5 +21,19 @@ function App() {
     </div>
   );
 }
+class App extends Component {    
+      render() {        
+          return (            
+              <h1 className="my-class">Coding Dojo</h1>,
+              <h3 className="todo">Things I need to do</h3>,
+              <ul>
+                <li>Learn React</li>
+                <li>Climb Mt Everest</li>
+                <li>Run Marathon</li>
+                <li>Walk the Dog</li>
+              </ul>
+          );    
+      }
+  }  
 
 export default App;
