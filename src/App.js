d69d5d32ce9697de2f2bf059745a3dd485f1a547
John Doe <elizabeth.lenzer1998@gmail.com>
2022-03-13 17:36:41 -0700

    fianl

--- src/App.js
+++ src/App.js
@@ -4,36 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1 className="my-class">Coding Dojo</h1>,
+        <h3 className="todo">Things I need to do</h3>,
+        <ul>
+          <li>Learn React</li>
+          <li>Climb Mt Everest</li>
+          <li>Run Marathon</li>
+          <li>Walk the Dog</li>
+        </ul>
     </div>
   );
 }
-class App extends Component {    
-      render() {        
-          return (            
-              <h1 className="my-class">Coding Dojo</h1>,
-              <h3 className="todo">Things I need to do</h3>,
-              <ul>
-                <li>Learn React</li>
-                <li>Climb Mt Everest</li>
-                <li>Run Marathon</li>
-                <li>Walk the Dog</li>
-              </ul>
-          );    
-      }
-  }  
+
 
 export default App;
